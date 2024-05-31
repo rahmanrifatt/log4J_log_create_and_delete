ef3517ebfa96fcc49169b0779205b6922872c93e
rahmanrifatt <74848658+rahmanrifatt@users.noreply.github.com>
2024-05-31 18:51:06 +0600

    Add files via upload

---
+++ logtst.java
@@ -0,0 +1,23 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class logtst {
+    // private static Logger logger = LogManager.getLogger(logtst.class.getName());
+
+    public static void main(String[] args) {
+        Logger logger = LogManager.getLogger(logtst.class.getName());
+
+                        logger.debug("myServer::serverSocket Closed");
+                        logger.info("eot find and server is closed");
+                        logger.debug("myServer::serverSocket Closed");
+                        logger.info("eot find and server is closed");
+                        logger.debug("myServer::serverSocket Closed");
+                        logger.info("eot find and server is closed");
+                        logger.debug("myServer::serverSocket Closed");
+                        logger.info("eot find and server is closed");
+                        logger.debug("myServer::serverSocket Closed");
+                        logger.error("kkkkkkkkkkkkkkkkkkkkkkkkk");
+        
+    }
+    
+}
